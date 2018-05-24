6090ad36fa0654cb94abc44e27a70a1b9c3b4f6e
manr <manr@users.noreply.github.com>
2018-05-24 17:18:14 +0200

    Form post.

--- broker-app/pages/form.js
+++ broker-app/pages/form.js
@@ -6,11 +6,12 @@ export default class extends React.Component {
 
     constructor(props) {
         super(props);
-        
-        this.state = {};
-        
+
+        this.state = {name: "", description: ""};
+
         // This binding is necessary to make `this` work in the callback
-        //this.refresh = this.refresh.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.submitForm = this.submitForm.bind(this);
     }
 
     /**
@@ -19,13 +20,41 @@ export default class extends React.Component {
      */
     static async getInitialProps({ req }) {
         if (req) {
-            return {name: ""}
+            return {name: "", description: ""}
         }
         else
         {
-            return {name: ""}
+            return {name: "", description: ""}
         }
     }
+
+    handleChange(event) {
+        if (!event)
+            return;
+
+        const value = event.target.value;
+        
+        if (event.target.name === 'tfName')
+            this.setState(prevState => ({
+                name: value
+            }));
+        else
+            this.setState(prevState => ({
+                description: value
+            }));
+    }
+
+    async submitForm(event) {
+        event.preventDefault();
+
+        const response = await axios.get('http://localhost:5000/api/send/' + this.state.name).catch((e) => e.response )
+        const result = response.data.request_id ? response.data.result : response.data;
+        
+        this.setState(prevState => ({
+            name: result
+        }));
+    }
+
 
     render() {
         return (
@@ -33,12 +62,26 @@ export default class extends React.Component {
                 <div>
                     Post form request
                 </div>
-                <div>
-                    
-                </div>
-                <div>
-                    <Link href="/index"><a>Back</a></Link>
-                </div>
+                <form onSubmit={this.submitForm}>
+                    <div>
+                        <label>
+                            Name:
+                            <input type="text" name="tfName" value={this.state.name} onChange={this.handleChange}/>
+                        </label>
+                    </div>
+                    <div>
+                    <label>
+                        Description:
+                        <input type="text" name="tfDescr" value={this.state.description} onChange={this.handleChange}/>
+                    </label>
+                    </div>
+                    <div>
+                        <input type="submit" value="Submit" />
+                    </div>
+                    <div>
+                        <Link href="/index"><a>Back</a></Link>
+                    </div>
+                </form>
             </div>
         )
     }
